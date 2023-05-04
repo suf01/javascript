c1289dc634a345fa84db1f4c4a81900a8afbb95a
sufiyan <90792596+suf01@users.noreply.github.com>
2023-05-04 15:26:27 +0530

    Create changing_innerHTML.js

---
+++ changing_innerHTML.js
@@ -0,0 +1,7 @@
+console.log("hellloooooo");
+
+let num = 10;
+let str = "the number is ";
+
+document.getElementById("p1").innerHTML = "changing this in js ";
+document.getElementById("p2").innerHTML = str + num;
